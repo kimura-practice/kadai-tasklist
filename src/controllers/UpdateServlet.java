43744d0ab2750d74fcc147744b47296aee12eae6
木村日和 <kimurahiyori0707@gmail.com>
2021-11-11 12:53:26 +0900

    修正

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -36,26 +36,26 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからタスクのIDを取得して
-            // 該当のIDのタスク1件のみをデータベースから取得
+            //セッションスコープからタスクのIDを取得して
+            //該当のIDのタスク1件のみをデータベースから取得
             Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
 
-            // フォームの内容を各フィールドに上書き
+            //フォームの内容を各フィールドに上書き
             String content = request.getParameter("content");
             t.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
-            // データベースを更新
+            //データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
             em.close();
 
-            // セッションスコープ上の不要になったデータを削除
+            //セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("tasks_id");
 
-            // indexページへリダイレクト
+            //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
         }
     }
